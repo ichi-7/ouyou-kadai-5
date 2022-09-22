e35f3933c7f25fe71433b3f6e0316fc54386b411
IkkanWatanabe <110508666+ichi-7@users.noreply.github.com>
2022-09-22 03:47:58 +0000

    [Finish] kadai-5

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   root :to =>"homes#top"
   devise_for :users
   
   get "home/about"=>"homes#about"
+  get 'relationships/followings'
+  get 'relationships/followers'
+  get "search" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
@@ -18,7 +18,5 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
